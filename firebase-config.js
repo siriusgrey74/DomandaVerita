23826d95ea07465638900ae29c05ff2b39566a3d
siriusgrey74 <43403665+siriusgrey74@users.noreply.github.com>
2025-08-09 16:59:25 +0200

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+
+// firebase-config.js - paste your config here
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyDPi2eXhnM4OJLhikPPnvrG7RWJDpk5JUs",
+  authDomain: "domandaverita.firebaseapp.com",
+  databaseURL: "https://domandaverita-default-rtdb.firebaseio.com",
+  projectId: "domandaverita",
+  storageBucket: "domandaverita.appspot.com",
+  messagingSenderId: "1010215229915",
+  appId: "1:1010215229915:web:960c3638629fd418beeffe"
+};
