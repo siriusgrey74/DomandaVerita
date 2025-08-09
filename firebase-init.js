5b3b6342aaeecd8e415b8903a32eb969c50709f6
siriusgrey74 <43403665+siriusgrey74@users.noreply.github.com>
2025-08-09 16:26:56 +0200

    Delete firebase-init.js

--- firebase-init.js
+++
@@ -1,7 +0,0 @@
-
-// firebase-init.js - initialize RTDB (compat SDK must be loaded before)
-(function(){
-  if(!window.firebase){ console.error('Firebase SDK not loaded'); return; }
-  if(!firebase.apps.length){ firebase.initializeApp(window.FIREBASE_CONFIG); }
-  window.DB = firebase.database();
-})();
